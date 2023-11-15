76c7c4e0c040588db056fc4ac07f91310c1f0d73
Lonelyhokage <149315355+Lonelyhokage@users.noreply.github.com>
2023-11-15 16:52:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     AccountSession = os.environ.get("AccountSession", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6900395318:AAGaqrRtHYcdzdoroctB9dr8dY2K6UypD4Y") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
     OWNER_ID = os.environ.get("OWNER_ID", "")
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
